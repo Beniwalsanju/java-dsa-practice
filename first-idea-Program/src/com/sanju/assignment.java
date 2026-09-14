0c13418b519df0c38ce8afda7dfdef706074cee5
beniwalsanju <sanjuboi0306@gmail.com>
2026-09-14 22:54:43 +0530

    This is my first java question

---
+++ first-idea-Program/src/com/sanju/assignment.java
@@ -0,0 +1,24 @@
+package com.sanju;
+
+import java.util.Scanner;
+
+public class assignment {
+    static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a year:");
+
+        int year = input.nextInt();
+
+        if (year % 400 == 0){
+            System.out.println("Leap Year");
+        } else if (year % 100 == 0) {
+            System.out.println("Not a Leap Year");
+        } else if (year % 4 ==0) {
+            System.out.println("Leap Year");
+        }else {
+            System.out.println("Not a Leap year");
+        }
+
+
+    }
+}
